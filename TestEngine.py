50a7a62bab692ab4627fe9c5437b026002208078
Drew Fisher <drew.m.fisher@gmail.com>
2012-10-22 21:48:41 -0700

    Fix the engine test

--- TestEngine.py
+++ TestEngine.py
@@ -5,32 +5,34 @@ from Unit import *
 from Card import *
 from WorkerCard import *
 from HeroCard import *
-from gw import cleanup_phase
+from Game import *
 
 class TestEngine(unittest.TestCase):
     def setUp(self):
-        self.players = {}
-        self.players[0] = Player()
-        self.players[1] = Player()
-        self.board = Board(3,7)
+        self.game = Game()
+        self.game.players = {}
+        self.game.players[0] = Player()
+        self.game.players[1] = Player()
+        self.game.players[1].set_direction(Player.FACING_LEFT)
+        self.game.board = Board(3,7)
 
     def test_cleanup_death(self):
         live_footman_card = Card('Footman')
         dead_footman_card = Card('Footman')
-        live_footman = Unit(live_footman_card, self.players[0])
-        dead_footman = Unit(dead_footman_card, self.players[0])
-        self.players[0].inplay.append(live_footman_card)
-        self.players[0].inplay.append(dead_footman_card)
+        live_footman = Unit(live_footman_card, self.game.players[0])
+        dead_footman = Unit(dead_footman_card, self.game.players[0])
+        self.game.players[0].inplay.append(live_footman_card)
+        self.game.players[0].inplay.append(dead_footman_card)
         dead_footman._hp = 0
 
-        self.board.grid[(1,1)] = live_footman
-        self.board.grid[(2,2)] = dead_footman
+        self.game.board.grid[(1,1)] = live_footman
+        self.game.board.grid[(2,2)] = dead_footman
 
-        cleanup_phase(self.players, self.board)
-        self.assertTrue((1,1) in self.board.grid)
-        self.assertFalse((2,2) in self.board.grid)
-        self.assertEqual(len(self.players[0].hand), 1)
-        self.assertEqual(len(self.players[0].inplay), 1)
+        self.game.cleanup_phase()
+        self.assertTrue((1,1) in self.game.board.grid)
+        self.assertFalse((2,2) in self.game.board.grid)
+        self.assertEqual(len(self.game.players[0].discard_pile), 1)
+        self.assertEqual(len(self.game.players[0].inplay), 1)
 
 
 if __name__ == "__main__":
