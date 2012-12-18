bceec33a9bc3d51a7a404b56671ed745cd2ed295
Eric Shrewsberry <eric.shrewsberry@gmail.com>
2012-12-18 06:00:16 -0500

    Forgot to include this in the original commit that added spell support to the UI

---
+++ TK_UIv2/TKCardInstance.py
@@ -0,0 +1,18 @@
+
+
+class TKCardInstance:
+    def __init__(self, name):
+        self.name = name
+        self.canvas_items = []
+
+    def paint(self, canvas, x_pixel, y_pixel):
+        item1_str = self.name
+        item1_opts = { "text": item1_str, "fill": "black", "font": ("ms serif", 14) }
+
+        item1 = canvas.create_text(x_pixel, y_pixel, **item1_opts)
+        self.canvas_items.append(item1)
+
+    def clear(self, canvas):
+        for item in self.canvas_items:
+            canvas.delete(item)
+        self.canvas_items = []
